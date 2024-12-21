75da2afc800d59e1438132e6f32bac2da4d2db3e
DaryaMashniuk <99687539+DaryaMashniuk@users.noreply.github.com>
2024-12-22 01:00:28 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://daryamashniuk.github.io/SPA-react-"
+  base: "https://daryamashniuk.github.io/SPA-react"
 })
